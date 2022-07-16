e31a4c4d52257ec918ee2f49b1d1cf03238b8153
tanj <junsheng.tan@eurecom.fr>
2022-07-16 20:01:45 +0300

    load and unmarshall yaml file

--- go.mod
+++ go.mod
@@ -2,22 +2,19 @@ module tan/policychampion
 
 go 1.18
 
+require github.com/open-policy-agent/opa v0.42.2
+
 require (
 	github.com/OneOfOne/xxhash v1.2.8 // indirect
 	github.com/agnivade/levenshtein v1.0.1 // indirect
 	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
-	github.com/open-policy-agent/opa v0.42.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rcrowley/go-metrics v0.0.0-20200313005456-10cdbea86bc0 // indirect
 	github.com/vektah/gqlparser/v2 v2.4.5 // indirect
 	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	github.com/yashtewari/glob-intersection v0.1.0 // indirect
-	github.com/yuin/goldmark v1.4.1 // indirect
-	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
-	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
-	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
-	golang.org/x/tools v0.1.11 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"reflect"
+
+	"gopkg.in/yaml.v3"
 
 	"github.com/open-policy-agent/opa/ast"
 	"github.com/open-policy-agent/opa/loader"
@@ -15,14 +19,27 @@ import (
 //     for i:= range
 // }
 
+func parseConfiguration(path string) map[string]interface{} {
+	rawFile, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatalf("Error when loading configuration file %v: %v", path, err)
+	}
+	parsedFile := make(map[string]interface{})
+
+	if err := yaml.Unmarshal(rawFile, &parsedFile); err != nil {
+		log.Fatalf("Error when parsing configuration file %v: %v", path, err)
+	}
+	return parsedFile
+}
+
 func getCompiler() *ast.Compiler {
 	path := []string{"./policies"}
 	policies, err := loader.AllRegos(path)
 	if err != nil {
-		fmt.Printf("Error during loading policies: %v \n", err)
+		log.Fatalf("Error during loading policies: %v \n", err)
 		return nil
 	} else if len(policies.Modules) == 0 {
-		fmt.Printf("No policy files found in: %v \n", path)
+		log.Fatalf("No policy files found in: %v \n", path)
 		return nil
 	}
 
@@ -36,58 +53,9 @@ func extractOptional(queryResult rego.ResultSet) []interface{} {
 }
 
 func main() {
-	// 	module := `
-	//     package main
 
-	//     import future.keywords
-
-	//     default allow := false
-
-	//     allow {
-	//         is_admin
-	//     }
-
-	//     optional[{key:msg}]{
-	//         is_admin
-
-	//         key :="example_key"
-	//         msg :="example message"
-	//     }
-
-	//     optional[{key:msg}]{
-	//         is_admin
-
-	//         key :="example_key2"
-	//         msg :="example message2"
-	//     }
-
-	//     is_admin {
-	//         "admin" in input.subject.groups
-	//     }
-	//     `
-
-	// 	module2 := `
-	//     package main
-
-	// default hello = false
-	// optional[{key:msg}]{
-	//     is_admin
-
-	//     key :="example_key3"
-	//     msg :="example message3"
-	// }
-	// optional[{key:msg}]{
-	//     is_admin
-
-	//     key :="example_key3"
-	//     msg :="example message4"
-	// }
-	// hello {
-	//     m := input.message
-	//     m == "world"
-	// }
-	//     `
-
+	yamlfile := parseConfiguration("./example.yaml")
+	fmt.Println(yamlfile["taskdefinition"].(map[string]interface{})["Properties"])
 	ctx := context.Background()
 
 	input := map[string]interface{}{
