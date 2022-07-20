7cff30d92d32715d36f850a14b048741747b01f8
tanj <junsheng.tan@eurecom.fr>
2022-07-20 19:47:56 +0300

    add command-line flag parsing

--- cmd/policychampion/policychampion.go
+++ cmd/policychampion/policychampion.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"flag"
 	util "tan/policychampion/internal"
 )
 
 func main() {
-	filename := "example.yaml"
-	rawFile := util.ReadFile(filename)
-	resultSet := util.EvalPolicy(rawFile)
+	// command-line flag parsing
+	inputPath := flag.String("i", "", "set path of input YAML file")
+	outputPath := flag.String("o", "updated"+*inputPath, "set path of output file")
+	regoNamespace := flag.String("namespace", "main", "set namespace of Rego package")
+	policyPath := flag.String("p", "policies", "set path of policy folder")
+	flag.Parse()
+
+	rawFile := util.ReadFile(*inputPath)
+	resultSet := util.EvalPolicy(rawFile, *regoNamespace, *policyPath)
 	updatedFile := util.ExecuteProhibitedRule(rawFile, resultSet)
 	updatedFile = util.ExecuteOptionalRule(updatedFile, resultSet)
+	updatedFile = util.ExecuteMandatoryRule(updatedFile, resultSet)
 
-	util.WriteFile("updated_"+filename, updatedFile)
+	util.WriteFile(*outputPath, updatedFile)
 }
