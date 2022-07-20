d4260844ed76db39d686c16f7ff02bbe64b5bbbe
tanj <junsheng.tan@eurecom.fr>
2022-07-20 18:55:09 +0300

    rule of type prohibited uses common ExecuteRule function to execute rules

--- internal/prohibited.go
+++ internal/prohibited.go
@@ -1,11 +1,25 @@
 package util
 
 import (
-	"log"
-
 	"github.com/open-policy-agent/opa/rego"
 	"gopkg.in/yaml.v3"
 )
+
+// given a YAML Node tree and a rego result
+// remove prohibited YAML nodes and add messages to a YAML Node as comment
+func removeNode(node *yaml.Node, hint map[string]interface{}) {
+	for key, msg := range hint {
+		elements := FindElements(node, key)
+		for _, element := range elements {
+
+			var emptyElement yaml.Node
+			emptyElement.Kind = yaml.ScalarNode
+			emptyElement.Value = "null"
+			emptyElement.HeadComment = AppendComment(element.HeadComment, msg.(string))
+			*element = emptyElement
+		}
+	}
+}
 
 // given raw byte data of a YAML, decision results returned by OPA
 // remove prohibited YAML nodes and append comments to YAML nodes
@@ -13,35 +27,5 @@ import (
 func ExecuteProhibitedRule(rawFile *[]byte, queryResult rego.ResultSet) *[]byte {
 	// extract prohibited result set
 	hints := ExtractRuleResult(queryResult, "prohibited")
-
-	var yamlNode yaml.Node
-	if err := yaml.Unmarshal(*rawFile, &yamlNode); err != nil {
-		log.Fatal(err)
-	}
-
-	// func: remove prohibited YAML nodes and add messages to a YAML Node as comment
-	removeNode := func(node *yaml.Node, key string, msg string) {
-		elements := FindElements(&yamlNode, key)
-		for _, element := range elements {
-
-			var emptyElement yaml.Node
-			emptyElement.Kind = yaml.ScalarNode
-			emptyElement.Value = "null"
-			emptyElement.HeadComment = AppendComment(element.HeadComment, msg)
-			*element = emptyElement
-		}
-
-	}
-
-	//loop each result and then execute prohibited rules
-	for _, hint := range hints {
-		for key, msg := range hint.(map[string]interface{}) {
-			removeNode(&yamlNode, key, msg.(string))
-		}
-	}
-	updatedConfiguration, err := yaml.Marshal(&yamlNode)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return &updatedConfiguration
+	return ExecuteRule(rawFile, hints, removeNode)
 }
