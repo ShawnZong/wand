7ba026a503c516d4e48eab14673680da80679dd1
tanj <junsheng.tan@eurecom.fr>
2022-07-20 18:45:27 +0300

    mandatory rule uses common ExecuteRule function to execute rules

--- internal/mandatory.go
+++ internal/mandatory.go
@@ -7,6 +7,42 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
+// given a YAML Node tree and a rego result
+// add template YAML to Node tree and append meesage as comment
+func appendMsgTemplate(node *yaml.Node, hint map[string]interface{}) {
+	// extract ket, msg and ref from a rego result
+	var key, msg, ref string
+	for mapKey, mapValue := range hint {
+		if mapKey == "templateRef" {
+			ref = mapValue.(string)
+		} else {
+			key = mapKey
+			msg = mapValue.(string)
+		}
+	}
+
+	// load reference template, because the template might be different
+	// we need to load the template separately each time
+	var refYAML yaml.Node
+	if err := yaml.Unmarshal(*ReadFile("../../" + ref), &refYAML); err != nil {
+		log.Fatalf("load ref file error: %v", err)
+	}
+
+	// search for specific YAML Nodes according to key
+	elements := FindElements(node, key)
+	for _, element := range elements {
+		// if the YAML Node is empty, replace empty Node directly with template Node
+		if len(element.Content) == 0 {
+			*element = *refYAML.Content[0]
+			element.HeadComment = AppendComment(element.HeadComment, msg)
+		} else {
+			// if the YAML Node is not empty, append template Node to existing Nodes
+			refYAML.Content[0].HeadComment = AppendComment(refYAML.Content[0].HeadComment, msg)
+			element.Content = append(element.Content, refYAML.Content...)
+		}
+	}
+}
+
 // given raw byte data of a YAML, decision results returned by OPA
 // append YAML template under specified key and add comments to YAML nodes
 // return raw byte data of a updated YAML
@@ -14,46 +50,5 @@ func ExecuteMandatoryRule(rawFile *[]byte, queryResult rego.ResultSet) *[]byte {
 	// extract mandatory result set
 	hints := ExtractRuleResult(queryResult, "mandatory")
 
-	var yamlNode yaml.Node
-	if err := yaml.Unmarshal(*rawFile, &yamlNode); err != nil {
-		log.Fatal(err)
-	}
-
-	// func: add optional messages to a YAML Node as comment
-	appendMsgTemplate := func(node *yaml.Node, key string, msg string, ref string) {
-		var refYAML yaml.Node
-		if err := yaml.Unmarshal(*ReadFile("../../" + ref), &refYAML); err != nil {
-			log.Fatalf("load ref file error: %v", err)
-		}
-		elements := FindElements(&yamlNode, key)
-		for _, element := range elements {
-			// if the YAML Node is empty, replace empty Node directly with template Node
-			if len(element.Content) == 0 {
-				*element = *refYAML.Content[0]
-				element.HeadComment = AppendComment(element.HeadComment, msg)
-			} else {
-				refYAML.Content[0].HeadComment = AppendComment(refYAML.Content[0].HeadComment, msg)
-				element.Content = append(element.Content, refYAML.Content...)
-			}
-		}
-	}
-
-	// loop each mandatory results and process corresponding nodes
-	for _, hint := range hints {
-		var key, msg, ref string
-		for mapKey, mapValue := range hint.(map[string]interface{}) {
-			if mapKey == "templateRef" {
-				ref = mapValue.(string)
-			} else {
-				key = mapKey
-				msg = mapValue.(string)
-			}
-		}
-		appendMsgTemplate(&yamlNode, key, msg, ref)
-	}
-	updatedConfiguration, err := yaml.Marshal(&yamlNode)
-	if err != nil {
-		log.Fatalf("marshal yaml error: %v", err)
-	}
-	return &updatedConfiguration
+	return ExecuteRule(rawFile, hints, appendMsgTemplate)
 }
