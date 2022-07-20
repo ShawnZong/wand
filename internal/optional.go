8abd020983c5f095da7394d43b31698c1474b713
tanj <junsheng.tan@eurecom.fr>
2022-07-20 18:55:32 +0300

    rule of type optional uses common ExecuteRule function to execute rules

--- internal/optional.go
+++ internal/optional.go
@@ -1,11 +1,20 @@
 package util
 
 import (
-	"log"
-
 	"github.com/open-policy-agent/opa/rego"
 	"gopkg.in/yaml.v3"
 )
+
+// given a YAML Node tree and a rego result
+// add optional messages to a YAML Node as comment
+func appendHint(node *yaml.Node, hint map[string]interface{}) {
+	for key, msg := range hint {
+		elements := FindElements(node, key)
+		for _, element := range elements {
+			element.HeadComment = AppendComment(element.HeadComment, msg.(string))
+		}
+	}
+}
 
 // given raw byte data of a YAML, decision results returned by OPA
 // append comments to YAML nodes
@@ -13,29 +22,5 @@ import (
 func ExecuteOptionalRule(rawFile *[]byte, queryResult rego.ResultSet) *[]byte {
 	// extract optional result set
 	hints := ExtractRuleResult(queryResult, "optional")
-
-	var yamlNode yaml.Node
-	if err := yaml.Unmarshal(*rawFile, &yamlNode); err != nil {
-		log.Fatal(err)
-	}
-
-	// func: add optional messages to a YAML Node as comment
-	appendHint := func(node *yaml.Node, key string, msg string) {
-		elements := FindElements(&yamlNode, key)
-		for _, element := range elements {
-			element.HeadComment = AppendComment(element.HeadComment, msg)
-		}
-	}
-
-	// for loop hints and then add optional messages to each Node
-	for _, hint := range hints {
-		for key, msg := range hint.(map[string]interface{}) {
-			appendHint(&yamlNode, key, msg.(string))
-		}
-	}
-	updatedConfiguration, err := yaml.Marshal(&yamlNode)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return &updatedConfiguration
+	return ExecuteRule(rawFile, hints, appendHint)
 }
